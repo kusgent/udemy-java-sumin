b3ed0f3a05997dade3b2a0b2bca3353385388c4b
Kustov Ilya <kusgent@yandex.ru>
2022-01-31 12:52:52 +0300

    Initial commit

---
+++ src/main/java/section01/MyFamily.java
@@ -0,0 +1,9 @@
+package section01;
+
+public class MyFamily {
+    public static void main(String[] args) {
+        System.out.println("Ilya");
+        System.out.println("Natalia");
+        System.out.println("Anastasia");
+    }
+}
