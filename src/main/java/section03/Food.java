b01a87dc46369466bff696d95e87655f4a465ca9
Kustov Ilya <kusgent@yandex.ru>
2022-02-02 21:40:33 +0300

    Add code to lesson 5

---
+++ src/main/java/section03/Food.java
@@ -0,0 +1,16 @@
+package section03;
+
+public class Food {
+    public static void main(String[] args) {
+        int purse = 550;
+        if (purse >= 500) {
+            System.out.println("Пицца");
+        } else if (purse >= 300) {
+            System.out.println("Шаурма");
+        } else if (purse >= 100) {
+            System.out.println("Гамбургер");
+        } else {
+            System.out.println("Доширак");
+        }
+    }
+}
---
+++ src/main/java/section03/Temp.java
@@ -0,0 +1,14 @@
+package section03;
+
+public class Temp {
+    public static void main(String[] args) {
+        int temp = 23;
+        if (temp > 25) {
+            System.out.println("Включить кондиционер");
+        } else if (temp < 22) {
+            System.out.println("Выключить кондиционер");
+        } else {
+            System.out.println("Отсутствие действия для кондиционера");
+        }
+    }
+}
