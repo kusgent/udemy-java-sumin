904a103ec9124b60a27633fa52b4b9fa96186b4f
Kustov Ilya <kusgent@yandex.ru>
2024-06-02 13:49:43 +0300

    Repeat numbers

---
+++ src/main/java/Repetition/Numbers.java
@@ -0,0 +1,23 @@
+package Repetition;
+
+public class Numbers {
+    public static void main(String[] args) {
+        int a;
+        int b;
+        int c;
+        a = 10;
+        b = 3;
+        c = a / b;
+        System.out.println("c = " + c);
+        c = a % b;
+        System.out.println("c = " + c);
+        c += 5;
+        System.out.println(c);
+        c -= 1;
+        System.out.println(c);
+        c *= 2;
+        System.out.println(c);
+        c /= 5;
+        System.out.println(c);
+    }
+}
