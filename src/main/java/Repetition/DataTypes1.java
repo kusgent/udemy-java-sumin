6b1fc37805d0a9085866c2ddcfc047696870f8f5
Kustov Ilya <kusgent@yandex.ru>
2024-07-03 23:16:07 +0300

    Repeat data types

---
+++ src/main/java/Repetition/DataTypes1.java
@@ -0,0 +1,10 @@
+package Repetition;
+
+public class DataTypes1 {
+    public static void main(String[] args) {
+        long seconds = 1000 * 24 * 60 * 60;
+        int speed = 300000;
+        long distance = seconds * speed;
+        System.out.println(distance);
+    }
+}
