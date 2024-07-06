50a8b36a9ef67c5f2da3e0d6cd8b5c7adeddfa23
Kustov Ilya <kusgent@yandex.ru>
2024-07-06 19:02:15 +0300

    Repeat data types

---
+++ src/main/java/Repetition/DataTypes2.java
@@ -0,0 +1,11 @@
+package Repetition;
+
+public class DataTypes2 {
+    public static void main(String[] args) {
+        float a = 3.5f;
+        float b = 4.7f;
+        float c;
+        c = (float) Math.sqrt(a * a + b * b);
+        System.out.println(c);
+    }
+}
